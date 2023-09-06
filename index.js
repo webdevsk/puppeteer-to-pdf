76fa73d756bd6f52973572d147613ef10a10fad5
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-09-06 20:39:03 +0600

    Changed CSS mode

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ const puppeteer = require('puppeteer')
   //   width: 1920,
   //   height: 1080,
   //   deviceScaleFactor: 1,
-  //   isMobile: false,
   //   isLandscape: true,
   // })
 
-  // go to page in resumeonly mode, wait for any network events to settle
+  // go to page in printonly mode, wait for any network events to settle
   await page.goto('http://localhost:3000?printonly=true', {
-    waitUntil: 'networkidle0',
+    waitUntil: 'networkidle2',
   })
+
+  await page.emulateMediaType('screen')
   // output to a local file
+
   const version = new Date().getTime()
 
   await page.pdf({
     path: `output/resume-v${version}.pdf`,
     // format: 'A4',
     printBackground: true,
-    // preferCSSPageSize: true,
+    preferCSSPageSize: true,
   })
 
   // close
